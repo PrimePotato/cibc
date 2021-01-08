518eeed4662a0f10190c1cb01ffada374708749c
Robert Cooper <Rob Cooper>
2021-01-08 17:11:49 +0000

    Adding more test cases

--- src/test/java/NAppearsNTimesTest.java
+++ src/test/java/NAppearsNTimesTest.java
@@ -11,15 +11,22 @@ class NAppearsNTimesTest {
 
     @Test
     void nthValue() {
-
-        for (int n : IntStream.range(0, 11).toArray()) {
+        for (int n : IntStream.range(0, 20).toArray()) {
             System.out.println(n + " " + NAppearsNTimes.nthValue(n) + " " + NAppearsNTimes.partialSum(n));
         }
     }
 
     @Test
+    void nthValueTestCase() {
+        int[] nValues = {1, 2, 3, 4, 5, 6, 10, 100, 1000};
+        int[] expected = {1, 2, 2, 3, 3, 3, 4, 14, 45};
+        int[] actual = Arrays.stream(nValues).map(NAppearsNTimes::nthValue).toArray();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     void verifyTestCases() {
-        int[] nValues = {10, 6, 7, 11, 15, 16, 100 ,10000, 1000, 21, 22};
+        int[] nValues = {10, 6, 7, 11, 15, 16, 100, 10000, 1000, 21, 22};
         int[] expected = {30, 14, 18, 35, 55, 61, 945, 942820, 29820, 91, 98};
         int[] actual = Arrays.stream(nValues).map(NAppearsNTimes::partialSum).toArray();
         assertArrayEquals(expected, actual);
