891d2b13a8eec1d66832d0ebae4f754941455c79
Robert Cooper <Rob Cooper>
2021-01-08 10:46:01 +0000

    'First'

---
+++ src/main/java/DiscreteProbability.java
@@ -0,0 +1,13 @@
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class DiscreteProbability {
+    private final List<Double> probabilities;
+    private final List<Double> values;
+
+    public double nextNum(){
+
+    }
+}
